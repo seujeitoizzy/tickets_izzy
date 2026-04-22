ecfae4a7883596abb4149f3b8716bc44c2e9a193
PabloDantasDev <pabloj72017@gmail.com>
2026-04-22 16:35:51 -0300

    atualziação cache

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Garante hash em todos os assets gerados
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
 })
